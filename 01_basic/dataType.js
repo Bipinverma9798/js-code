6baf65cff6d67e1561b8c1de8eba4bb6e6a71f3e
Bipinverma9798 <bipinverma9798hz@gmail.com>
2025-03-31 20:30:06 +0000

    sec video

---
+++ 01_basic/dataType.js
@@ -0,0 +1,17 @@
+"use strict";//treat all js code newer version
+
+//alert(3 + 3)//we are usin node js,not browser
+
+let name = "Bipin"
+let age = 3
+let isLoggedIn = false
+
+//number =>2 to the power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null => standlone value
+//undefined =>
+//symbol => unique
+//object
+
---
+++ 01_basic/variable.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail ="bipinverma9798hz@gamil.com"
+var accountPassword = 54321
+accountCity ="ranchi"
+ let accountState;
+//console.log("bipin")
+/*
+note
+prefer not usd var
+because of issue in block scope and functional scop
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
